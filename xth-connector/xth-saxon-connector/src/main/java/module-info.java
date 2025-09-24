7b460c1a67d9a293f58efc8716b2b39d4d5e4d23
Adam Retter <adam.retter@googlemail.com>
2025-09-24 10:08:43 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! First version

--- xth-connector/xth-saxon-connector/src/main/java/module-info.java
+++ xth-connector/xth-saxon-connector/src/main/java/module-info.java
@@ -1,4 +1,7 @@
 module xth.connector.impl.saxon {
+
+    provides com.evolvedbinary.xth.connector.spi.ConnectorProvider with com.evolvedbinary.xth.connector.saxon.SaxonConnectorProvider;
+
     requires xth.connector.api;
     requires xth.common;
     requires java.xml;
--- xth-parser/xth-fots-31-parser/src/main/java/module-info.java
+++ xth-parser/xth-fots-31-parser/src/main/java/module-info.java
@@ -1,4 +1,9 @@
 module xth.parser.impl.fots31 {
+
+    provides com.evolvedbinary.xth.parser.spi.TestSuiteParserProvider with com.evolvedbinary.xth.parser.fots31.FOTS31ParserProvider;
+
+    opens org.w3._2010._09.qt_fots_catalog to jakarta.xml.bind;
+
     requires xth.parser.api;
     requires xth.common;
     requires com.fasterxml.aalto;
